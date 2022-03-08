5a225bbc8dc21eeb8c4b7dfd77f6ea1a95e4bd4c
Anderson <anderson.os88@hotmail.com>
2022-03-08 17:56:06 -0300

    tarefa mod 11

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome e seu Generoo masculino/feminino");
+        String nome = sc.next();
+
+        String[] genero = nome.split(",");
+
+        List<String> lista = new ArrayList<String>(List.of(genero));
+
+        System.out.println(lista);
+
+
+
+        }
+    }
+
+
+
